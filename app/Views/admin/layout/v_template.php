623bdd1d3fb96ee0fe59da68479839e23aea06b6
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2021-03-29 11:24:48 +0800

    setting view admin template

---
+++ app/Views/admin/layout/v_template.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?= $this->include('admin/layout/partials/temp_head'); ?>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  <!-- include navbar -->
+  <?= $this->include('admin/layout/partials/temp_navbar'); ?>
+  
+  <!-- include sidebar -->
+  <?= $this->include('admin/layout/partials/temp_sidebar'); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+        <?= $this->renderSection('content-header'); ?>
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main Content -->
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <?= $this->renderSection('content'); ?>
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.1.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+  <!-- include sidebar -->
+  <?= $this->include('admin/layout/partials/temp_script'); ?>
+</body>
+</html>
